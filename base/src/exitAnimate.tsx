4de349ec274cadb14b18f02d05f994b0b81ddf92
wy2010344 <wangyang2010344@foxmail.com>
2023-11-28 18:45:59 +0800

    some bug fix

--- base/src/exitAnimate.tsx
+++ base/src/exitAnimate.tsx
@@ -122,10 +122,13 @@ export function useExitAnimate<V>(list: readonly V[], getKey: (v: V) => any, {
         }
       }
     }
-    if (destroyPromises.length && onExitComplete) {
+    const onExitWait = mode == 'wait' && thisAddList.length != 0
+    if (destroyPromises.length && (onExitComplete || onExitWait)) {
       const allDestroyPromise = Promise.all(destroyPromises)
-      allDestroyPromise.then(onExitComplete)
-      if (mode == 'wait' && thisAddList.length != 0) {
+      if (onExitComplete) {
+        allDestroyPromise.then(onExitComplete)
+      }
+      if (onExitWait) {
         allDestroyPromise.then(function () {
           //将本次更新全部标记为展示.
           let n = 0
