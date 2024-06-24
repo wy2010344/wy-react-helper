439f1d74c0adb68313d2ce6a3462205d049bf5fd
wy2010344 <wangyang2010344@foxmail.com>
2024-06-24 13:45:20 +0800

    just published

--- dom/src/useMatchMedia.ts
+++ dom/src/useMatchMedia.ts
@@ -1,13 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useMemo } from "react";
 import { useChange } from "wy-react-helper";
 
 export function useMatchMedia(pattern: string) {
-  const [matchMedia, setMatchMedia] = useChange(true);
+  const match = useMemo(() => window.matchMedia(pattern), [pattern])
+  const [matchMedia, setMatchMedia] = useChange(match.matches);
   useEffect(() => {
     function heightChange(ev: MediaQueryListEvent) {
       setMatchMedia(ev.matches);
     }
-    const match = window.matchMedia(pattern);
+    setMatchMedia(match.matches);
+    match.addEventListener("change", heightChange);
+    return () => {
+      match.removeEventListener("change", heightChange);
+    };
+  }, [match]);
+  return matchMedia;
+}
+
+/**
+ * 服务器端没有matchMedia
+ * @param pattern 
+ * @param def 
+ * @returns 
+ */
+export function useMatchMediaServer(pattern: string, def = false) {
+  const [matchMedia, setMatchMedia] = useChange(def);
+  useLayoutEffect(() => {
+    const match = window.matchMedia(pattern)
+    function heightChange(ev: MediaQueryListEvent) {
+      setMatchMedia(ev.matches);
+    }
     setMatchMedia(match.matches);
     match.addEventListener("change", heightChange);
     return () => {
