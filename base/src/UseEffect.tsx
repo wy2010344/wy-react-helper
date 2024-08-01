9054d506cb854452da26a778bfd05eabb06982f8
wy2010344 <wangyang2010344@foxmail.com>
2024-08-01 18:24:18 +0800

    add effect destroy

--- base/src/UseEffect.tsx
+++ base/src/UseEffect.tsx
@@ -1,4 +1,5 @@
 import React, { DependencyList, EffectCallback, useEffect } from 'react'
+import { arrayFunToOneOrEmpty, EmptyFun, FalseType } from 'wy-helper'
 
 export function UseEffect({
   effect,
@@ -9,4 +10,24 @@ export function UseEffect({
 }) {
   useEffect(effect, deps)
   return <></>
+}
+let globalVS: ((e: any) => void)[] | FalseType = undefined
+export function addEffectDestroy(fun: (e: EmptyFun) => void) {
+  if (globalVS) {
+    globalVS.push(fun)
+  } else {
+    throw new Error("必须在effect里执行")
+  }
+}
+export function useHookEffect(
+  effect: EmptyFun,
+  deps?: DependencyList
+) {
+  useEffect(() => {
+    const list: EmptyFun[] = []
+    globalVS = list
+    effect()
+    globalVS = undefined
+    return arrayFunToOneOrEmpty(list)
+  }, deps)
 }
