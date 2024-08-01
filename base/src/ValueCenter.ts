e97dd3e816d9c13453719097cce082f748818f5d
wy2010344 <wangyang2010344@foxmail.com>
2024-08-01 18:24:18 +0800

    add effect destroy

--- base/src/ValueCenter.ts
+++ base/src/ValueCenter.ts
@@ -1,10 +1,10 @@
 import { useMemo } from "react";
-import { ValueCenter, valueCenterOf } from "wy-helper";
+import { emptyArray, ValueCenter, valueCenterOf } from "wy-helper";
 
 export function useValueCenter<T>(init: () => T): ValueCenter<T> {
   return useMemo(() => {
     return valueCenterOf(init());
-  }, []);
+  }, emptyArray);
 }
 export function useValueCenterWith<T>(): ValueCenter<T | undefined>;
 export function useValueCenterWith<T>(v: T): ValueCenter<T>;
