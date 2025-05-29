9ccc5fddb3f6be77b65216fc967e57faf7e7b307
wy2010344 <wangyang2010344@foxmail.com>
2025-05-29 21:00:45 +0800

    fix content editable

--- dom/src/useContentEditable.tsx
+++ dom/src/useContentEditable.tsx
@@ -1,87 +1,153 @@
-import React, { useEffect, useMemo, useReducer, useRef } from "react"
-import { flushSync } from "react-dom"
-import { contentEditableText, EditRecord, ContentEditableModel, fixScroll, appendRecord } from "wy-dom-helper/contentEditable"
-import { emptyArray } from "wy-helper"
-import { HookRender } from "wy-react-helper"
+import React, { useEffect, useMemo, useReducer, useRef } from "react";
+import { flushSync } from "react-dom";
+import { ComponentValueCache, subscribeEventListener } from "wy-dom-helper";
+import {
+  contentEditableText,
+  EditRecord,
+  ContentEditableModel,
+  fixScroll,
+  appendRecord,
+  MbRange,
+  initContentEditableModel,
+  getCurrentEditRecord,
+} from "wy-dom-helper/contentEditable";
+import {
+  anyStoreTransform,
+  emptyArray,
+  GetValue,
+  StoreTransform,
+} from "wy-helper";
+import {
+  HookRender,
+  useStoreTriggerRender,
+  useValueCenter,
+  useValueCenterWith,
+  useVersion,
+} from "wy-react-helper";
 
-export type EditAction = {
-  type: "input"
-  record: EditRecord
-} | {
-  type: "undo"
-} | {
-  type: "redo"
-} | {
-  type: "reset"
-  record: EditRecord
-}
+const triggerArg = {
+  triggerType: flushSync,
+};
+export function useControlledEditable(
+  value: string,
+  onChange: (v: string, range: MbRange) => void
+) {
+  const model = useValueCenter(() => initContentEditableModel(value));
 
-
-function reducer(model: ContentEditableModel, action: EditAction): ContentEditableModel {
-  if (action.type == "input") {
-    return appendRecord(model, action.record)
-  } else if (action.type == "undo") {
-    const cdx = model.currentIndex
-    if (cdx) {
-      return {
-        currentIndex: cdx - 1,
-        history: model.history
-      }
-    }
-  } else if (action.type == "redo") {
-    const ndx = model.currentIndex + 1
-    if (ndx < model.history.length) {
-      return {
-        currentIndex: ndx,
-        history: model.history
-      }
-    }
-  } else if (action.type == "reset") {
+  const store = useMemo(() => {
     return {
-      currentIndex: 0,
-      history: [action.record],
+      get: model.get.bind(model),
+      set(v: ContentEditableModel) {
+        model.set(v);
+        const c = getCurrentEditRecord(v);
+        onChange(c.value, c.range);
+      },
     };
-  }
-  return model
+  }, emptyArray);
+  return [useStoreTriggerRender(model, triggerArg), store] as const;
 }
-
-export function useContentEditable<T>(t: T, initFun: (t: T) => ContentEditableModel) {
-  const [value, _dispatch] = useReducer(reducer, t, initFun)
-  const dispatch: typeof _dispatch = function (...args) {
-    flushSync(() => {
-      _dispatch(...args)
-    })
-  }
+/**
+ * @todo 使用ValueCenter或Signal,再flushSync的方式同步回state,
+ *  再做成受控组件,需要useLayoutEffect去同步两处状态
+ * @param t
+ * @param initFun
+ * @returns
+ */
+export function useContentEditable(value: ContentEditableModel) {
   const current = useMemo(() => {
-    return value.history[value.currentIndex]
-  }, [value.history, value.currentIndex])
+    return value.history[value.currentIndex];
+  }, [value.history, value.currentIndex]);
   return {
     current,
-    value,
-    dispatch,
     renderContentEditable(args: {
-      readonly?: boolean
-      noFocus?: boolean
-    }, renderContent: (ref: React.RefObject<HTMLElement>) => JSX.Element) {
-      return <HookRender key={current.value} render={function () {
-        const ref = useRef<HTMLElement>(null)
-        useEffect(() => {
-          const div = ref.current!
-          if (args.readonly) {
-            div.contentEditable = 'false'
-          } else {
-            div.contentEditable = contentEditableText + ''
-          }
-        }, [args.readonly])
-        useEffect(() => {
-          if (args.noFocus) {
-            return
-          }
-          const div = ref.current!
-          fixScroll(div, current)
-        }, emptyArray)
-        return renderContent(ref)
-      }} />
+      readonly?: boolean;
+      noFocus?: boolean;
+      renderContent(ref: React.RefObject<HTMLElement>): JSX.Element;
+    }) {
+      return (
+        <HookRender
+          key={current.value}
+          render={function () {
+            const ref = useRef<HTMLElement>(null);
+            useEffect(() => {
+              const div = ref.current!;
+              if (args.readonly) {
+                div.contentEditable = "false";
+              } else {
+                div.contentEditable = contentEditableText + "";
+              }
+            }, [args.readonly]);
+            useEffect(() => {
+              if (args.noFocus) {
+                return;
+              }
+              const div = ref.current!;
+              fixScroll(div, current);
+            }, emptyArray);
+            return args.renderContent(ref);
+          }}
+        />
+      );
+    },
+  };
+}
+
+export type TriggerTime = "onInput" | "onBlur";
+/**
+ * T 模型值
+ * V 从元素中取出来的值
+ * @param param0
+ * @param deps
+ */
+export function useContentEditablePure<T, V = T>(
+  {
+    getCache,
+    triggerTime = "onInput",
+    value,
+    setValue,
+    transform = anyStoreTransform as StoreTransform<T, V>,
+  }: {
+    getCache: GetValue<ComponentValueCache<HTMLElement, V>>;
+    triggerTime?: TriggerTime;
+    value: T;
+    setValue: (v: T) => void;
+    transform?: StoreTransform<T, V>;
+  },
+  /**
+   * 生成cache的依赖
+   */
+  deps: readonly any[] = emptyArray
+) {
+  const [version, updateVersion] = useVersion();
+  const cacheRef = useRef<ComponentValueCache<HTMLElement, V>>();
+  useEffect(() => {
+    cacheRef.current = getCache();
+  }, deps);
+  useEffect(() => {
+    const cache = cacheRef.current;
+    if (cache) {
+      const div = cache.input;
+      const update = () => {
+        updateVersion();
+        //是否需要实时更新?flushSync?
+        transform.fromComponent(cache.get(), setValue);
+      };
+      if (triggerTime == "onInput") {
+        return subscribeEventListener(div, "input", update);
+      }
+      if (triggerTime == "onBlur") {
+        return subscribeEventListener(div, "blur", update);
+      }
+    } else {
+      console.warn("not generate cache");
     }
-  }
+  }, [triggerTime, ...deps]);
+  useEffect(() => {
+    const cache = cacheRef.current;
+    if (cache) {
+      if (transform.shouldChange(value, cache.get())) {
+        cache.set(transform.toComponentValue(value));
+      }
+    }
+  }, [value, version, ...deps]);
 }
